9f3d200ee28b375bc829e0cb419bbdf910d7e6d0
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-13 23:13:46 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,23 @@
 print("Basic Calculator")
 print(".................")
+num_1 = float(input( "Enter a number: "))
+op = input("Enter operation: ")
+num_2 = float(input("Enter another number: "))
+
+if op == "+":
+    print(float(num_1) + float(num_2))
+elif op == "-":
+    print(float(num_1) - float(num_2))
+elif op == "*":
+    print(float(num_1) * float(num_2))
+elif op == "/":
+    print(float(num_1) / float(num_2))
+else:
+    print("Invalid operation.")
+
+
+
+#result = float(num_1) + float(num_2)
+#print("Answer: " + str(result))
+
+
