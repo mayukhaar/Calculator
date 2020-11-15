a3d08dc0a0113960e352a63959a191fadb5cfb02
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-15 11:48:04 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,23 +1,48 @@
-print("Basic Calculator")
+print("BASIC CALCULATOR")
 print(".................")
-num_1 = float(input( "Enter a number: "))
-op = input("Enter operation: ")
-num_2 = float(input("Enter another number: "))
+print("These are instructions on how to use this calculator ")
+print("                     ")
 
-if op == "+":
-    print(float(num_1) + float(num_2))
-elif op == "-":
-    print(float(num_1) - float(num_2))
-elif op == "*":
-    print(float(num_1) * float(num_2))
-elif op == "/":
-    print(float(num_1) / float(num_2))
-else:
-    print("Invalid operation.")
+repeat = "Y"
 
 
+while repeat == "Y":
 
-#result = float(num_1) + float(num_2)
-#print("Answer: " + str(result))
+    op = input("Enter operation: ")
+    num_1 = float(input( "Enter a number: "))
+    num_2 = float(input("Enter another number: "))
+    if op == "+":
+        add = float(num_1) + float(num_2)
+        print("Answer: " + str(add))
+        repeat = input("Do you want to enter another calculation (Y/N): ")
+    elif op == "-":
+        subtract = float(num_1) - float(num_2)
+        print("Answer: " + str(subtract))
+        repeat = input("Do you want to enter another calculation (Y/N): ")
+    elif op == "*":
+        multiply = float(num_1) * float(num_2)
+        print("Answer: " + str(multiply))
+        repeat = input("Do you want to enter another calculation (Y/N): ")
+    elif op == "/":
+        divide = float(num_1) / float(num_2)
+        print("Answer: " + str(divide))
+        repeat = input("Do you want to enter another calculation (Y/N): ")
+    else:
+        print("Invalid operation.")
+
+print("Bye! Have a nice day:)")
+
+#repeat = input("Do you want to enter another calculation (Y/N: ")
+
+
+#else:
+#print("jdhsf")
+    #if repeat == "N":
+       # print("Bye! Have a nice day :)")
+    #else:
+      #  print ("Invalid operation.")
+      #  print(repeat)
+
+
 
 
